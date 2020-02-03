2a51e6ac820d172b2c6ec2481ac2bfe79b2f42fc
Katherine Zhdanova <katherine.zhdanova8@gmail.com>
2020-02-03 15:47:01 +0200

    bank updated

--- js/10_bank.js
+++ js/10_bank.js
@@ -1,15 +1,35 @@
 (function () {
     'use strict';
 
-    let sum = prompt("Укажите сумму вклада");
-    let months = prompt("На какое количество месяцев?")
-    let percent = prompt("Под какой годовой процент?")
+    let sum = Number(prompt("Укажите сумму вклада"));
+    let months = Number(prompt("На какое количество месяцев?"))
+    let percent = Number(prompt("Под какой годовой процент?"))
 
-    let finalProfit = Math.round(sum * (percent / months) / 100 * months);
+    let newSum = sum;
 
-    alert(`Вам будет начислено ${finalProfit} грн`);
+    if (months >= 12) {
 
+        let fullYears = Math.floor(months / 12);
+        let restMonths = months % 12;
 
+        for (let i = 1; i <= fullYears; i++) {
+            newSum += (newSum * percent / 100);
+        }
+        for (let j = 1; j <= restMonths; j++) {
+            newSum += (newSum * (percent / 12) / 100);
+        }
+
+    } else {
+        newSum = sum;
+        for (let a = 1; a <= months; a++) {
+            newSum += (newSum * (percent / 12) / 100);
+
+        }
+    }
+
+    let profit = newSum - sum;
+
+    alert(`Вам будет начислено ${profit.toFixed(2)} грн`);
 })()
 
 
